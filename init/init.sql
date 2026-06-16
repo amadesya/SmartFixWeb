5f921c4e9dc22a83776227c61ca46afe4b9e6c5b
amadesya <ultrashitband@gmail.com>
2026-06-16 21:31:46 +0300

    feat: add database and user creation logic in init.sql

--- init/init.sql
+++ init/init.sql
@@ -19,6 +19,39 @@
 -- Table structure for table `__efmigrationshistory`
 --
 
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: smartfix
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `__efmigrationshistory`
+--
+
+-- Создаем базу, если ее нет
+CREATE DATABASE IF NOT EXISTS smartfix;
+
+-- Создаем пользователя, который может подключаться ТОЛЬКО из сети Docker
+-- (используем имя сервиса или маску сети, если Docker настроен)
+CREATE USER IF NOT EXISTS 'smartfix_user'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+
+-- Даем права ТОЛЬКО на нужную базу
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX ON smartfix.* TO 'smartfix_user'@'%';
+
+FLUSH PRIVILEGES;
+
 DROP TABLE IF EXISTS `__efmigrationshistory`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -554,4 +587,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-16 21:15:53
+-- Dump completed on 2026-06-16 21:29:58
